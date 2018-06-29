bea8a62e1b4c77db0df5f4c9643256f52993602d
Kirill Kotov <admin@ircclub.net>
2018-06-29 15:01:45 +0400

    first commit

---
+++ image_optim.sh
@@ -0,0 +1,22 @@
+#!/bin/env bash
+IFS=$'\n'
+imagelist=$(find /root/images_test/optimized/images -type f \! -name "\.\*" \! -path "/root/images_test/optimized/images/backup/\*")
+db="/var/lib/image_optim/amazingy.db"
+
+# Create DB: sqlite3 /var/lib/image_optim/amazingy.db  "create table md5sum (id TEXT);"
+
+for a in $imagelist
+do
+  md5=$(md5sum $a | awk '{ print $1}')
+  result=$(sqlite3 $db "select id from md5sum where id='"$md5"';")
+ if [[ `echo "$result" |grep $md5` ]]
+  then
+    echo "Exist"
+    continue
+  else
+    image_optim $a
+    md5=$(md5sum $a | awk '{ print $1}')
+    sqlite3 $db "insert into md5sum (id) values ('"$md5"');"
+  fi
+done
+
